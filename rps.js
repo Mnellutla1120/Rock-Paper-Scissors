7c55ea64f64bdcf05f90a64617511e069038b79e
Mnellutla1120 <162918815+Mnellutla1120@users.noreply.github.com>
2024-08-13 18:26:04 -0400

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,24 @@
+let humanResp = prompt("Rock, Paper, Scissors, Shoot!");
+
+
+function getHumanChoice(humanResp){
+return humanResp; 
+}
+
+function getComputerChoice(){
+  let compAnswer = ""; 
+  let choice = getRandomInt(3); 
+  if (choice === 0){
+    compAnswer = "Rock";
+  }
+  else if (choice === 1){
+compAnswer = "Paper";
+  }
+  else if (choice === 2){
+compAnswer = "Scissors";
+  }
+  return compAnswer; 
+
+}
+
 
